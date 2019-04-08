30c01ee75d6877b85ea8d93923eaf2734804fc7c
DeepanjanTTN <deepanjan.ghosh@tothenew.com>
2019-04-08 10:03:56 +0530

    hoc added

---
+++ hoc-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ChildComp from './childcomp';
+import './App.css';
+import AuthProvider from './AuthProvider'; 
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <AuthProvider>
+        <div>
+          <h1> parent component passing isAuthenticated property through context</h1>
+          <br/>
+          <ChildComp />
+        </div>
+      </AuthProvider>
+
+      
+
+    );
+  }
+}
+
+export default App;
